1537363543c8f4c0b9ac3988cf94b44c6d0e0d5a
DharaniSharavanan <33505743+Dharani1996@users.noreply.github.com>
2017-12-14 15:53:38 +0530

    Add files via upload

---
+++ Book/Ebook.java
@@ -0,0 +1,37 @@
+package org.book;
+
+public class Ebook extends Book {
+	private String downloadURL;
+	private float size;
+		public Ebook() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+		
+		public Ebook(Integer id, String title, String author, double price, long iSBNnumber, String publisher,
+				String edision) {
+			super(id, title, author, price, iSBNnumber, publisher, edision);
+			// TODO Auto-generated constructor stub
+		}
+
+		public Ebook(String downloadURL, float size) {
+			super();
+			this.downloadURL = downloadURL;
+			this.size = size;
+		}
+
+		public String getDownloadURL() {
+			return downloadURL;
+		}
+		public void setDownloadURL(String downloadURL) {
+			this.downloadURL = downloadURL;
+		}
+		public float getSize() {
+			return size;
+		}
+		public void setSize(float size) {
+			this.size = size;
+		}
+		
+
+}
