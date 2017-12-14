bd8d5be2c04c19831396587fb5021d06669362d9
DharaniSharavanan <33505743+Dharani1996@users.noreply.github.com>
2017-12-14 15:52:19 +0530

    Create book.java

---
+++ Book/book.java
@@ -0,0 +1,82 @@
+package org.book;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="Author_details",discriminatorType=DiscriminatorType.STRING)
+public class Book {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String title;
+	private String author;
+	private double price;
+	private long ISBNnumber;
+	private String publisher;
+	private String edision;
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Book(Integer id, String title, String author, double price, long iSBNnumber, String publisher,
+			String edision) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.price = price;
+		ISBNnumber = iSBNnumber;
+		this.publisher = publisher;
+		this.edision = edision;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public long getISBNnumber() {
+		return ISBNnumber;
+	}
+	public void setISBNnumber(long iSBNnumber) {
+		ISBNnumber = iSBNnumber;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public String getEdision() {
+		return edision;
+	}
+	public void setEdision(String edision) {
+		this.edision = edision;
+	}
+	
+}
